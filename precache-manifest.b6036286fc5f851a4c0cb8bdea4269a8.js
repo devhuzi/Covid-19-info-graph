45e6a23c8d2e1331f1978b917ee4f27c02819c26
DevHuzi <hux3fa@gmail.com>
2020-06-26 22:43:45 +1000

    Updates

---
+++ precache-manifest.b6036286fc5f851a4c0cb8bdea4269a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65fc10efac88bc34653bf5e8a77de4d5",
+    "url": "/Covid-19-info-graph/index.html"
+  },
+  {
+    "revision": "6c06e60b8510353792b5",
+    "url": "/Covid-19-info-graph/static/css/main.68558285.chunk.css"
+  },
+  {
+    "revision": "6fb3a8df53d24ead3dee",
+    "url": "/Covid-19-info-graph/static/js/2.81996775.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid-19-info-graph/static/js/2.81996775.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c06e60b8510353792b5",
+    "url": "/Covid-19-info-graph/static/js/main.9c626955.chunk.js"
+  },
+  {
+    "revision": "580b7e4b801dfbc14427",
+    "url": "/Covid-19-info-graph/static/js/runtime-main.1098259f.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid-19-info-graph/static/media/image.d7265326.png"
+  }
+]);
